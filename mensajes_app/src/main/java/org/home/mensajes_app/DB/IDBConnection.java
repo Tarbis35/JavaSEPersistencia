96facd851bc3743cb6bd3d8bd9ff7bb9b451ab16
Leonardo Vanegas <jleonardo.vanegas@outlook.es>
2020-03-29 16:21:25 -0500

    Primera fase persistencia en base de datos en java

--- mensajes_app/src/main/java/org/home/mensajes_app/DB/IDBConnection.java
+++ mensajes_app/src/main/java/org/home/mensajes_app/DB/IDBConnection.java
@@ -6,14 +6,11 @@ import java.sql.DriverManager;
 import static org.home.mensajes_app.DB.DataBase.*;
 
 public interface IDBConnection {
-    default Connection connectionToDB(){
+    static Connection connectionToDB(){
         Connection connection = null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection= DriverManager.getConnection(URL+DB+TIMEZONE,USER,PASSWORD);
-            if(connection != null){
-                System.out.println("se estableció la conección");
-            }
         }catch (Exception e){
             e.printStackTrace();
         }finally {
